371a654242bcf17a828ca6aa82452aded1e55ac6
Saudamini Keshav <saudamini.keshav@gmail.com>
2020-07-06 10:23:45 -0700

    Added StartQuiz button and styling

--- script.js
+++ script.js
@@ -1,3 +1,10 @@
 
-// let homeDiv = document.querySelector("#homeDiv");
-// homeDiv.setAttribute("style", "height:500px;");
+//Region HTML element creation
+var cardBody = document.querySelector("card-body");
+var cardTitle = document.querySelector("card-title");
+var startQuizButton = document.querySelector("#startQuizButton");
+// End region 
+
+// Region event listener
+
+// End region 
