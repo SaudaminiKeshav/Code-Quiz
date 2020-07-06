b37358e29dcb3790f1e6fcd622a12cc51f998f14
Saudamini Keshav <saudamini.keshav@gmail.com>
2020-07-06 14:21:16 -0700

    Created html elements of quiz question in javascript and added quizContent json

--- script.js
+++ script.js
@@ -1,4 +1,96 @@
 
+// Region create quiz content json 
+var quizContent = [
+    {
+        "question": "Inside which HTML element do we put the JavaScript?",
+        "options": [
+            "<js>",
+            "<script>",
+            "<javascript>",
+            "<scripting>"
+        ]
+    },
+    {
+        "question": "Where is the correct place to insert a JavaScript?",
+        "options": [
+            "The <body> section",
+            "The <head> section",
+            "Both the <head> section and the <body> section are correct",
+            "None of the above"
+        ]
+    },
+    {
+        "question": "What is the correct syntax for referring to an external script called \"xxx.js\"?",
+        "options": [
+            "<script name = \"xxx.js\">",
+            "<script href = \"xxx.js\">",
+            "<script src = \"xxx.js\">",
+            "None of the above"
+        ]
+    },
+    {
+        "question": "The external JavaScript file must contain the <script> tag.",
+        "options": [
+            "True",
+            "False"
+        ]
+    },
+    {
+        "question": "How do you write \"Hello World\" in an alert box?",
+        "options": [
+            "alertBox(\"Hello World\");",
+            "alert(\"Hello World\");",
+            "msg(\"Hello World\");",
+            "msgBox(\"Hello World\");"
+        ]
+    },
+    {
+        "question": "How do you create a function in JavaScript?",
+        "options": [
+            "function:myFunction()",
+            "function myFunction()",
+            "function = myFunction()"
+        ]
+    },
+    {
+        "question": "How do you call a function named \"myFunction\"?",
+        "options": [
+            "myFunction()",
+            "call function myFunction()",
+            "call myFunction()"
+        ]
+    },
+    {
+        "question": "How to write an IF statement in JavaScript?",
+        "options": [
+            "if (i==5)",
+            "if i = 5 then",
+            "if i = 5",
+            "if i == 5 then"
+        ]
+    },
+    {
+        "question": "How do you round the number 7.25, to the nearest integer?",
+        "options": [
+            "rnd(7.25)",
+            "round(7.25)",
+            "Math.round(7.25)",
+            "Math.rnd(7.25)"
+        ]
+    },
+    {
+        "question": "Which event occurs when the user clicks on an HTML element?",
+        "options": [
+            "onChange",
+            "onClick",
+            "onmouseover",
+            "onmouseclick"
+        ]
+    }
+];
+
+//End region
+
 //Region HTML element creation
 var cardBody = document.querySelector(".card-body");
 var cardTitle = document.querySelector("h4");
@@ -8,10 +100,12 @@ var startQuizButton = document.querySelector("#start-quiz-button");
 
 // Region event listener
 startQuizButton.addEventListener('click', function(){
-    cardTitle.textContent = " ";
-    cardBodyText.textContent = " ";
+    cardBody.removeChild(cardTitle);
+    cardBody.removeChild(cardBodyText);
+    
     cardBody.appendChild(timerLabel);
     timerLabel.appendChild(timeCountdownValue);
+    createAndDisplayQuizQuestions(cardBody);
 })
 // End region 
 
@@ -34,3 +128,44 @@ if(TotalTime<0){
 }, 1000);
 // End region 
 
+// Region quiz question creation 
+function createAndDisplayQuizQuestions(cardBody){
+
+    // Region create row and colums for quiz options 
+    var rowDiv = document.createElement("div");
+    rowDiv.setAttribute("class", "row rowBody py-5");
+
+    var colDiv1 = document.createElement("div");
+    colDiv1.setAttribute("class", "colBody col-lg-6");
+
+    var colDiv2 = document.createElement("div");
+    colDiv2.setAttribute("class", "colBody col-lg-6");
+    // End region 
+
+    // Region display quiz question 
+    var questionPTag = document.createElement("p");
+    questionPTag.setAttribute("class", "questionPTag");
+    // End region 
+
+    generateQuizQuestion(colDiv1, colDiv2, questionPTag);
+
+    // Append question p tag, row and column div tags to body 
+    cardBody.appendChild(questionPTag);
+    rowDiv.appendChild(colDiv1);
+    rowDiv.appendChild(colDiv2);
+    cardBody.appendChild(rowDiv)
+}
+
+function generateQuizQuestion(colDiv1, colDiv2, questionPTag){
+    createAndDisplayQuizOptions(colDiv1, "test text");
+}
+
+function createAndDisplayQuizOptions(colDiv, quizOption){
+    var alertDiv = document.createElement("div");
+    alertDiv.setAttribute("class", "alert alert-primary");
+    alertDiv.setAttribute("role", "alert");
+    alertDiv.textContent = quizOption;
+
+    colDiv.appendChild(alertDiv);
+}
+// End region 
