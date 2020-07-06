0366108cac4f1cac3cef3df7e4d03cde0c5da31d
Saudamini Keshav <saudamini.keshav@gmail.com>
2020-07-06 10:44:35 -0700

    Added event listener to Start Quiz button

--- script.js
+++ script.js
@@ -1,10 +1,14 @@
 
 //Region HTML element creation
 var cardBody = document.querySelector("card-body");
-var cardTitle = document.querySelector("card-title");
-var startQuizButton = document.querySelector("#startQuizButton");
+var cardTitle = document.querySelector("h4");
+var cardBodyText = document.querySelector("p");
+var startQuizButton = document.querySelector("#start-quiz-button");
 // End region 
 
 // Region event listener
-
+startQuizButton.addEventListener('click', function(){
+    console.log(cardTitle.innerHTML);
+    cardBodyText.textContent = " ";
+})
 // End region 
