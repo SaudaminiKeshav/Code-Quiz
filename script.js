ffb8c6fb6b6aeeaeb0508d610dd651e56b1828e4
Saudamini Keshav <saudamini.keshav@gmail.com>
2020-07-06 12:19:48 -0700

    Added timer function and display timer count down

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 
 //Region HTML element creation
-var cardBody = document.querySelector("card-body");
+var cardBody = document.querySelector(".card-body");
 var cardTitle = document.querySelector("h4");
 var cardBodyText = document.querySelector("p");
 var startQuizButton = document.querySelector("#start-quiz-button");
@@ -8,7 +8,29 @@ var startQuizButton = document.querySelector("#start-quiz-button");
 
 // Region event listener
 startQuizButton.addEventListener('click', function(){
-    console.log(cardTitle.innerHTML);
+    cardTitle.textContent = " ";
     cardBodyText.textContent = " ";
+    cardBody.appendChild(timerLabel);
+    timerLabel.appendChild(timeCountdownValue);
 })
 // End region 
+
+// Region timer 
+var TotalTime = 100;
+var timerLabel = document.createElement("p");
+timerLabel.setAttribute("class", "timer-label");
+timerLabel.textContent = "Time remaining: "
+
+var timeCountdownValue = document.createElement("h5");
+timeCountdownValue.setAttribute("class", "timeCountdownValue");
+
+var timerFunc = setInterval(function(){
+timeCountdownValue.textContent = TotalTime;
+console.log(`Timer running`);
+TotalTime--;
+if(TotalTime<0){
+    clearInterval(timerFunc);
+}
+}, 1000);
+// End region 
+
