e7dbf6daa3fbb959ec544dfbf2a097aa52cffb2d
Saudamini Keshav <saudamini.keshav@gmail.com>
2020-07-06 15:11:42 -0700

    Parsed the json and populated the quiz question and options. Also add another field in the json for correct answer

--- script.js
+++ script.js
@@ -1,96 +1,4 @@
 
-// Region create quiz content json 
-var quizContent = [
-    {
-        "question": "Inside which HTML element do we put the JavaScript?",
-        "options": [
-            "<js>",
-            "<script>",
-            "<javascript>",
-            "<scripting>"
-        ]
-    },
-    {
-        "question": "Where is the correct place to insert a JavaScript?",
-        "options": [
-            "The <body> section",
-            "The <head> section",
-            "Both the <head> section and the <body> section are correct",
-            "None of the above"
-        ]
-    },
-    {
-        "question": "What is the correct syntax for referring to an external script called \"xxx.js\"?",
-        "options": [
-            "<script name = \"xxx.js\">",
-            "<script href = \"xxx.js\">",
-            "<script src = \"xxx.js\">",
-            "None of the above"
-        ]
-    },
-    {
-        "question": "The external JavaScript file must contain the <script> tag.",
-        "options": [
-            "True",
-            "False"
-        ]
-    },
-    {
-        "question": "How do you write \"Hello World\" in an alert box?",
-        "options": [
-            "alertBox(\"Hello World\");",
-            "alert(\"Hello World\");",
-            "msg(\"Hello World\");",
-            "msgBox(\"Hello World\");"
-        ]
-    },
-    {
-        "question": "How do you create a function in JavaScript?",
-        "options": [
-            "function:myFunction()",
-            "function myFunction()",
-            "function = myFunction()"
-        ]
-    },
-    {
-        "question": "How do you call a function named \"myFunction\"?",
-        "options": [
-            "myFunction()",
-            "call function myFunction()",
-            "call myFunction()"
-        ]
-    },
-    {
-        "question": "How to write an IF statement in JavaScript?",
-        "options": [
-            "if (i==5)",
-            "if i = 5 then",
-            "if i = 5",
-            "if i == 5 then"
-        ]
-    },
-    {
-        "question": "How do you round the number 7.25, to the nearest integer?",
-        "options": [
-            "rnd(7.25)",
-            "round(7.25)",
-            "Math.round(7.25)",
-            "Math.rnd(7.25)"
-        ]
-    },
-    {
-        "question": "Which event occurs when the user clicks on an HTML element?",
-        "options": [
-            "onChange",
-            "onClick",
-            "onmouseover",
-            "onmouseclick"
-        ]
-    }
-];
-
-//End region
-
 //Region HTML element creation
 var cardBody = document.querySelector(".card-body");
 var cardTitle = document.querySelector("h4");
@@ -130,16 +38,17 @@ if(TotalTime<0){
 
 // Region quiz question creation 
 function createAndDisplayQuizQuestions(cardBody){
+    //Get quiz content from json
+    var quizObj = JSON.parse(JSON.stringify(quizContent));
+    console.log(quizContent.length)
 
     // Region create row and colums for quiz options 
     var rowDiv = document.createElement("div");
-    rowDiv.setAttribute("class", "row rowBody py-5");
+    rowDiv.setAttribute("class", "row rowBody py-5 justify-content-center");
 
-    var colDiv1 = document.createElement("div");
-    colDiv1.setAttribute("class", "colBody col-lg-6");
+    var colDiv = document.createElement("div");
+    colDiv.setAttribute("class", "colBody col-lg-6");
 
-    var colDiv2 = document.createElement("div");
-    colDiv2.setAttribute("class", "colBody col-lg-6");
     // End region 
 
     // Region display quiz question 
@@ -147,17 +56,21 @@ function createAndDisplayQuizQuestions(cardBody){
     questionPTag.setAttribute("class", "questionPTag");
     // End region 
 
-    generateQuizQuestion(colDiv1, colDiv2, questionPTag);
+    var i = 0;
+    generateQuizQuestion(colDiv, questionPTag, quizObj[i]);
 
     // Append question p tag, row and column div tags to body 
     cardBody.appendChild(questionPTag);
-    rowDiv.appendChild(colDiv1);
-    rowDiv.appendChild(colDiv2);
+    rowDiv.appendChild(colDiv);
     cardBody.appendChild(rowDiv)
 }
 
-function generateQuizQuestion(colDiv1, colDiv2, questionPTag){
-    createAndDisplayQuizOptions(colDiv1, "test text");
+function generateQuizQuestion(colDiv, questionPTag, quizObj){
+        questionPTag.textContent = quizObj.question;
+        for(var i= 0; i<quizObj.options.length; i++){
+            createAndDisplayQuizOptions(colDiv, quizObj.options[i]);
+        }
+
 }
 
 function createAndDisplayQuizOptions(colDiv, quizOption){
@@ -165,7 +78,109 @@ function createAndDisplayQuizOptions(colDiv, quizOption){
     alertDiv.setAttribute("class", "alert alert-primary");
     alertDiv.setAttribute("role", "alert");
     alertDiv.textContent = quizOption;
-
     colDiv.appendChild(alertDiv);
 }
 // End region 
+
+
+// Region create quiz content json 
+var quizContent = [
+    {
+        "question": "Inside which HTML element do we put the JavaScript?",
+        "options": [
+            "<js>",
+            "<script>",
+            "<javascript>",
+            "<scripting>"
+        ], 
+        "rightOptions": "<script>"
+    },
+    {
+        "question": "Where is the correct place to insert a JavaScript?",
+        "options": [
+            "The <body> section",
+            "The <head> section",
+            "Both the <head> section and the <body> section are correct",
+            "None of the above"
+        ],
+        "rightOptions": "The <body> section"
+    },
+    {
+        "question": "What is the correct syntax for referring to an external script called \"xxx.js\"?",
+        "options": [
+            "<script name = \"xxx.js\">",
+            "<script href = \"xxx.js\">",
+            "<script src = \"xxx.js\">",
+            "None of the above"
+        ],
+        "rightOptions": "<script src = \"xxx.js\">"
+    },
+    {
+        "question": "The external JavaScript file must contain the <script> tag.",
+        "options": [
+            "True",
+            "False"
+        ],
+        "rightOptions": "False"
+    },
+    {
+        "question": "How do you write \"Hello World\" in an alert box?",
+        "options": [
+            "alertBox(\"Hello World\");",
+            "alert(\"Hello World\");",
+            "msg(\"Hello World\");",
+            "msgBox(\"Hello World\");"
+        ],
+        "rightOptions": "alert(\"Hello World\");"
+    },
+    {
+        "question": "How do you create a function in JavaScript?",
+        "options": [
+            "function:myFunction()",
+            "function myFunction()",
+            "function = myFunction()"
+        ],
+        "rightOptions": "function myFunction()"
+    },
+    {
+        "question": "How do you call a function named \"myFunction\"?",
+        "options": [
+            "myFunction()",
+            "call function myFunction()",
+            "call myFunction()"
+        ],
+        "rightOptions": "myFunction()"
+    },
+    {
+        "question": "How to write an IF statement in JavaScript?",
+        "options": [
+            "if (i==5)",
+            "if i = 5 then",
+            "if i = 5",
+            "if i == 5 then"
+        ],
+        "rightOptions": "if (i==5)"
+    },
+    {
+        "question": "How do you round the number 7.25, to the nearest integer?",
+        "options": [
+            "rnd(7.25)",
+            "round(7.25)",
+            "Math.round(7.25)",
+            "Math.rnd(7.25)"
+        ],
+        "rightOptions": "Math.round(7.25)"
+    },
+    {
+        "question": "Which event occurs when the user clicks on an HTML element?",
+        "options": [
+            "onChange",
+            "onClick",
+            "onmouseover",
+            "onmouseclick"
+        ],
+        "rightOptions": "onClick"
+    }
+];
+
+//End region
