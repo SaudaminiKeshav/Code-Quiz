872be51c9127e0a93c7edd4319331fdc51f6c3bd
Saudamini Keshav <saudamini.keshav@gmail.com>
2020-07-06 20:01:41 -0700

    Added logic to change the quiz question on clicking options

--- script.js
+++ script.js
@@ -1,101 +1,3 @@
-
-//Region HTML element creation
-var cardBody = document.querySelector(".card-body");
-var cardTitle = document.querySelector("h4");
-var cardBodyText = document.querySelector("p");
-var startQuizButton = document.querySelector("#start-quiz-button");
-// End region 
-
-// Region event listener
-startQuizButton.addEventListener('click', function () {
-    cardBody.removeChild(cardTitle);
-    cardBody.removeChild(cardBodyText);
-
-    cardBody.appendChild(timerLabel);
-    timerLabel.appendChild(timeCountdownValue);
-    createAndDisplayQuizQuestions(cardBody);
-})
-// End region 
-
-// Region timer 
-var TotalTime = 100;
-var timerLabel = document.createElement("p");
-timerLabel.setAttribute("class", "timer-label");
-timerLabel.textContent = "Time remaining: "
-
-var timeCountdownValue = document.createElement("h5");
-timeCountdownValue.setAttribute("class", "timeCountdownValue");
-
-var timerFunc = setInterval(function () {
-    timeCountdownValue.textContent = TotalTime;
-    TotalTime--;
-    if (TotalTime < 0) {
-        clearInterval(timerFunc);
-    }
-}, 1000);
-// End region 
-
-// Region quiz question creation 
-function createAndDisplayQuizQuestions(cardBody) {
-    //Get quiz content from json
-    var quizObj = JSON.parse(JSON.stringify(quizContent));
-
-    // Region create row and colums for quiz options 
-    var rowDiv = document.createElement("div");
-    rowDiv.setAttribute("class", "row rowBody py-5 justify-content-center");
-
-    var colDiv = document.createElement("div");
-    colDiv.setAttribute("class", "colBody col-lg-6");
-
-    // End region 
-
-    // Region display quiz question 
-    var questionPTag = document.createElement("p");
-    questionPTag.setAttribute("class", "questionPTag");
-    // End region 
-
-    var i = 0;
-    generateQuizQuestion(colDiv, questionPTag, quizObj[i]);
-
-    // Append question p tag, row and column div tags to body 
-    cardBody.appendChild(questionPTag);
-    rowDiv.appendChild(colDiv);
-    cardBody.appendChild(rowDiv)
-}
-
-function generateQuizQuestion(colDiv, questionPTag, quizObj) {
-    questionPTag.textContent = quizObj.question;
-    for (var i = 0; i < quizObj.options.length; i++) {
-        createAndDisplayQuizOptions(colDiv, quizObj.options[i], quizObj.correctOption);
-    }
-   
-}
-
-function createAndDisplayQuizOptions(colDiv, quizOption, correctOption) {
-    var alertDiv = document.createElement("div");
-    alertDiv.setAttribute("class", "alert alert-primary");
-    alertDiv.setAttribute("role", "alert");
-    alertDiv.textContent = quizOption;
-    colDiv.appendChild(alertDiv);
-    addQuizOptionsClickListener(alertDiv, quizOption, correctOption)
-}
-// End region 
-
-// Region quiz options click listener 
-function addQuizOptionsClickListener(alertDiv,  selectedOption, correctOption){
-    alertDiv.addEventListener('click', function(){
-        if(selectedOption == correctOption){
-            console.log("Correct answer");
-        }else{
-            console.log("Wrong answer");
-            if(TotalTime != 0){
-                TotalTime = TotalTime - 5;
-            }
-        }
-    })
-}
-// End region 
-
 // Region create quiz content json 
 var quizContent = [
     {
@@ -197,3 +99,138 @@ var quizContent = [
 ];
 
 //End region
+
+var quizQuestionIndex = 0;
+var totalQuestions = 10;
+
+//Region HTML element creation
+var cardBody = document.querySelector(".card-body");
+var cardTitle = document.querySelector("h4");
+var cardBodyText = document.querySelector("p");
+var startQuizButton = document.querySelector("#start-quiz-button");
+// End region 
+
+// Region create html elements 
+// Create row and colums for quiz options 
+var rowDiv = document.createElement("div");
+rowDiv.setAttribute("class", "row rowBody py-5 justify-content-center");
+
+var colDiv = document.createElement("div");
+colDiv.setAttribute("class", "colBody col-lg-6");
+
+// Display quiz question 
+var questionPTag = document.createElement("p");
+questionPTag.setAttribute("class", "questionPTag");
+
+  //Get quiz content from json
+  var quizObj = JSON.parse(JSON.stringify(quizContent));
+
+// Event listener
+startQuizButton.addEventListener('click', function () {
+    cardBody.removeChild(cardTitle);
+    cardBody.removeChild(cardBodyText);
+
+    cardBody.appendChild(timerLabel);
+    timerLabel.appendChild(timeCountdownValue);
+    createAndDisplayQuizQuestions(cardBody);
+})
+// End region 
+
+// Region timer 
+var TotalTime = 100;
+var timerLabel = document.createElement("p");
+timerLabel.setAttribute("class", "timer-label");
+timerLabel.textContent = "Time remaining: "
+
+var timeCountdownValue = document.createElement("p");
+timeCountdownValue.setAttribute("class", "timeCountdownValue");
+
+var timerFunc = setInterval(function () {
+    timeCountdownValue.textContent = TotalTime;
+    TotalTime--;
+    if (TotalTime < 0) {
+        clearInterval(timerFunc);
+    }
+}, 1000);
+// End region 
+
+// Region quiz question creation 
+function createAndDisplayQuizQuestions(cardBody) {
+
+    generateQuizQuestion(quizQuestionIndex);
+
+    // Append question p tag, row and column div tags to body 
+    cardBody.appendChild(questionPTag);
+    rowDiv.appendChild(colDiv);
+    cardBody.appendChild(rowDiv)
+}
+
+function generateQuizQuestion(quizQuestionIndex) {
+   
+
+    questionPTag.textContent = quizObj[quizQuestionIndex].question;
+    for (var i = 0; i < quizObj[quizQuestionIndex].options.length; i++) {
+        createAndDisplayQuizOptions(colDiv, quizObj[quizQuestionIndex].options[i], quizObj[quizQuestionIndex].correctOption, i);
+    }
+}
+
+function createAndDisplayQuizOptions(colDiv, quizOption, correctOption, divCount) {
+    var alertDiv = document.createElement("div");
+    alertDiv.setAttribute("class", "alert alert-primary");
+    alertDiv.setAttribute("role", "alert");
+    alertDiv.setAttribute("id", `alertDiv${divCount}`);
+    console.log(divCount);
+    alertDiv.textContent = quizOption;
+    colDiv.appendChild(alertDiv);
+    addQuizOptionsClickListener(alertDiv, quizOption, correctOption)
+}
+// End region 
+
+// Region quiz options click listener 
+function addQuizOptionsClickListener(alertDiv, selectedOption, correctOption) {
+    alertDiv.addEventListener('click', function () {
+        if (selectedOption == correctOption) {
+            console.log("Correct answer");
+            alertDiv.setAttribute("class", "alert alert-success");
+            ++quizQuestionIndex
+            displayNextQuestion();
+        } else {
+            console.log("Wrong answer");
+            alertDiv.setAttribute("class", "alert alert-danger");
+            if (TotalTime != 0) {
+                TotalTime = TotalTime - 10;
+            }
+            ++quizQuestionIndex
+            displayNextQuestion();
+        }
+    })
+}
+// End region 
+
+// Region display next question 
+function displayNextQuestion(){
+if(colDiv.hasChildNodes()){
+console.log(colDiv.childNodes);
+
+// for(var i = 0; i<quizObj[quizQuestionIndex].options.length; i++){
+//     var alertDiv = document.querySelector(`#alertDiv${i}`);
+//     alertDiv.textContent = quizObj[quizQuestionIndex].options[i];
+//     resetAlertDivClass(alertDiv);
+//     addQuizOptionsClickListener(alertDiv, quizObj[quizQuestionIndex].options[i], quizObj[quizQuestionIndex].correctOption);
+// }
+ 
+    questionPTag.textContent = quizObj[quizQuestionIndex].question;
+
+   
+    // addQuizOptionsClickListener(alertDiv, quizOption, correctOption)
+
+    // for (var i = 0; i < quizObj[quizQuestionIndex].options.length; i++) {
+    //     createAndDisplayQuizOptions(colDiv, quizObj[quizQuestionIndex].options[i], quizObj[quizQuestionIndex].correctOption, i);
+    // }
+}
+}
+// End region 
+
+function resetAlertDivClass(alertDiv){
+    alertDiv.setAttribute("class", "alert alert-primary");
+}
